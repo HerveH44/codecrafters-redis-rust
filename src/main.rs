d0c1e6363d97ab59b566de9fe2e36cbc62740cc9
HerveH44 <herve.huneau@gmail.com>
2024-05-26 18:55:12 +0200

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                _stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                _stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
