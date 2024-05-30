18bea7e7eb57246381d8f3e3cb5169cd632fa881
HerveH44 <herve.huneau@gmail.com>
2024-05-30 13:50:34 +0200

    fix read stream

--- src/connection.rs
+++ src/connection.rs
@@ -1,14 +1,21 @@
 use anyhow::{anyhow, Result};
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    str,
+};
 
 pub fn handle_connection(mut stream: impl Read + Write) -> Result<()> {
-    let mut buffer = String::new();
-    match stream.read_to_string(&mut buffer)? {
+    let mut buffer: [u8; 1024] = [0; 1024];
+    match stream.read(&mut buffer)? {
         0 => Err(anyhow!("Connection closed")),
         _ => {
-            buffer.lines().filter(|l| *l == "PING").for_each(|_| {
-                stream.write_all(b"+PONG\r\n").unwrap();
-            });
+            let client_command = str::from_utf8(&buffer)?;
+            client_command
+                .lines()
+                .filter(|l| *l == "PING")
+                .for_each(|_| {
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                });
             Ok(())
         }
     }
