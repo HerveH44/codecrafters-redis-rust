39fc8d3a2f7ef7a1ca76f925d423d4511af09e80
HerveH44 <herve.huneau@gmail.com>
2024-05-31 08:22:52 +0200

    use u8 buffer

--- src/connection.rs
+++ src/connection.rs
@@ -1,21 +1,23 @@
 use anyhow::Result;
-use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader};
 
 pub async fn handle_connection<Reader, Writer>(reader: Reader, mut writer: Writer) -> Result<()>
 where
     Reader: AsyncRead + Unpin,
     Writer: AsyncWrite + Unpin,
 {
-    let mut line = String::new();
     let mut reader = BufReader::new(reader);
 
     loop {
-        if let Ok(bytes_read) = reader.read_line(&mut line).await {
+        let mut buf: [u8; 1024] = [0; 1024];
+        if let Ok(bytes_read) = reader.read(&mut buf).await {
             if bytes_read == 0 {
                 break Ok(());
             }
-            writer.write_all(b"+PONG\r\n").await.unwrap();
+            let line = std::str::from_utf8(&buf).unwrap();
+            for _ in line.lines().filter(|l| *l == "PING") {
+                writer.write_all(b"+PONG\r\n").await.unwrap();
+            }
         }
-        line.clear();
     }
 }
